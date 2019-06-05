94e6cd77e4e956f626e104e01dd357c807038e63
Emma <emma.green.software@gmail.com>
2019-06-04 22:08:45 -0400

    add content

--- videlish/views.py
+++ videlish/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from videlish.models import Recipe
+
+class FeedView(View):
+    def get(self, request):
+        all_recipes = Recipe.objects.all()
+        context = {'recipes': all_recipes}
+        return render(request, 'feed.html', context)
+
+
+    # def post(self, request):
+        # Code block for POST request
